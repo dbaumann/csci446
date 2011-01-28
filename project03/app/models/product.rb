fc25dcece8a34e11398f2e42df23767a4f09ddee
Daniel Baumann <dbaumann@mines.edu>
2011-01-28 09:27:23 -0700

    Iteration B2: Added the store controller, implemented initial model and view fucntionality to go with it.

---
+++ project03/app/models/product.rb
@@ -0,0 +1,37 @@
+class Product < ActiveRecord::Base
+  
+  #these look like method calls outside of a method definition
+  validates_presence_of :title, :description, :image_url
+  
+  validates_numericality_of :price
+  validate :price_must_be_at_least_a_cent
+  
+  validates_uniqueness_of :title
+  
+  #what if this was an uploaded file? could we validate the binary data?
+  validates_format_of :image_url,
+                      :with => %r{\.(gif|jpg|png)$}i, #regex in case-insensitive mode
+                      #what about for an email address?
+                      :message => "must be a URL for GIF, JPG, or PNG image"
+  
+  validates_length_of :title, :minimum => 10,
+                    :message => "your product description must be atleast 10 " +
+                      "characters long."
+                      
+  def self.find_products_for_sale
+    #all matching rows as opposed to a certain number of them, ordered by title
+    find(:all, :order => 'title')
+    #don't need to specify the context for find; it's the product ofcourse
+    #one benefit of having the model code in a class that extends the ORM
+  end
+
+protected
+
+  #the specification of a business rule in plain english
+  #another reason why validation belongs in the model: validations are often
+  #business rules, and business rules are enforced in the model
+  def price_must_be_at_least_a_cent
+    errors.add(:price, 'should be atleast 0.01') if price.nil? or price < 0.01
+  end
+  
+end
---
+++ project03/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = 'f8016069c74690f0ecfd2d309a302c6eb53b7c940e415e8b5bd4c3bf9f6c4cb5d654bcc3138cd5548dd2f07a9e441aa20fc0bb847989338a3e4595d78e1f3712';
