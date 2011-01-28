9b9f4ec97f8a1040ff48a0e661ad187cfcb501bc
Daniel Baumann <dbaumann@mines.edu>
2011-01-28 14:01:08 -0700

    Iteration C3: Added code to handle the error raised when user attempts to add to the cart a product that doesn't exist.

--- project03/app/controllers/store_controller.rb
+++ project03/app/controllers/store_controller.rb
@@ -10,6 +10,16 @@ class StoreController < ApplicationController
     #this is to add a PRODUCT; the controller doesn't care about
     #how the cart deals with the items it's been given
     @cart.add_product(product)
+  
+  #remember, a rescue applies to the entire function;
+  #kind of like an else, but for functions
+  rescue ActiveRecord::RecordNotFound
+  #class-level access to an inner class, which exists as a class constant of ActiveRecord
+    
+    #is logger really an attribute?
+    logger.error("Attempt to access invalid product #{params[:id]}")
+    flash[:notice] = "Invalid product"  #think of flash as a one-time session
+    redirect_to :action => 'index'
   end
   
 private
