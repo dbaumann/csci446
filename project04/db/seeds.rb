08f450e58dc813d5842a38877750dc09bef8149b
Daniel Baumann <dbaumann@mines.edu>
2011-02-05 19:57:24 -0700

    Scaffolded /article, added some seed data, made some initial adjustments to the index view.

--- project04/db/seeds.rb
+++ project04/db/seeds.rb
@@ -5,3 +5,84 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+
+article_1 = Hash.new
+article_1[:title] = "Top 5 Architectural Design Software Products"
+article_1[:author_name] = "Dan Fargo"
+article_1[:body] = <<HERE
+If you are browsing for an architectural design software to begin redesigning your potential residence, or likely to guide you with your college works, the subsequent guide may help one to make a good choice concerning the perfect architectural design software to suit your needs.
+
+Even while I was student in architecture, I used to be pretty confused with the options which i had at that time. It was 2003 and my second year, so as everyone learned a software, I needed to pick one myself. After researching like crazy thru forums, and talking to my colleagues I began to use Archicad, since it seemed the most effective fit for what we had to do... That appeared to work fine, until I needed to develop more complicated 3D objects, also to rotate them easily... During those times, Archicad was a bit dumber in that area, so I switched to Allplan, and stayed with it ever since.
+
+Enough, about me, let's talk about what can be your options today, as the current market in architectural design software is a whole lot more diverse than a several years back.
+
+1. ArchiCAD
+
+The primary recommendation I have for rookies, is ArchiCAD, and that is as this software, was, but still is among the most intuitive ones with regards to producing and adjusting floor plans in my view. What meaning, is that if you're a young architect or designer you can easily discover the most significant commands and use them immediately to digitally draw whatever you want. You can also obtain a free trial version for the software.
+
+2. Chief Architect
+
+Is actually a professional 3D architectural home design software. It includes a built in automated building tools to make home design, remodeling, interior design and kitchen & bath design as simple as it gets. You can find why Chief Architect could be the home design software software product of preference for 2D and 3D design for a lot of US architects. This software also has an free trial offer, so if you think this can be a better suited option, give it a shot.
+
+3. Punch Software
+
+Self intitled America's #1 Home & Landscape Design Software, this application combines the power of NexGen with the stunning realism of photographic quality from LightWorks, enabling you to create realistic home designs yourself, or for your customers. You can check it on their official page.
+
+4. Autocad Architecture
+
+AutoCAD Architecture software program is the package of AutoCAD designed for architects. Features developed for architectural drafting help you build designs and documents more effectively, inside the familiar AutoCAD environment. Certainly, you can try this one too before you buy it.
+
+5. Vectorworks Architect
+
+This is my personal favorite, and I left it towards the end, simply because is quite complex, and although it's difficult to sit and learn your way around it, the program will deliver fast and extraordinary results after you have used it for some time. But as I was saying, this is for tech nerds, and in all likelihood not really the first option for a newbie.
+
+This group of architectural designs software should supply you with a basic guidance regarding what is available today out there and what you could buy for your future designs.
+
+You can learn more about architectural design software and other design programs and resources at our website http://www.archtopia.com
+
+Article Source: http://EzineArticles.com/?expert=Dan_Fargo
+HERE
+
+article_2 = Hash.new
+article_2[:title] = "Offshore Software Outsourcing"
+article_2[:author_name] = "Rakhee Chowdhary"
+article_2[:body] = <<HERE
+Offshore software application development outsourcing has led to vegetative industry growth around the globe. Software needs to undergo a step-by-step process before it is available for general use to leverage best industry practices at minimum cost, meeting international standards and industry norms. Nowadays, everyone of us, an individual or an enterprise wants to be at the top, but just gaining the top positioning won't do; retaining and maintaining that position is of utmost consideration. Outsourcing software applications to offshore companies has strong impulse to monetize spending of business players.
+
+Software outsourcing gathered strength with increasing number of sources available in present IT-edge, requirements delivery in possible least time, less skilled personnel available, discrete communication and inefficient management of various modules within a large enterprise. These corporate players started looking and relying on third party for better management at lesser cost and remain competitive in the global marketplace. These are the few factors that led big companies to outsource their software application development to offshore companies where they find time zone difference from eastern countries most attractive, cheaper human capital, rich resources, and skilled professional and low processes cost. Market analysis shows gradual penetration of investors in Indian market and other developing countries. We find increased productive, managed easy solutions where full control remains in the hand of outsourcing companies.
+
+40% of the Fortune 500 companies including GE, Lucent, Oracle, Microsoft, Motorola are leveraging the outsourcing model. The list is growing depending upon the onsite and the outsource resources available. Offshore Software Outsourcing is the most leading area of outsourcing. Cost saving by adopting this model can vary from 40% to 70%.
+
+Manipulating overall strategy (considering failure and success) we find overall the offshore outsourcing campaign is a success. Objective is to maximize the ROI, remain competitive and reduce upbringing cost.
+
+Remember offshoring software outsourcing is not always the solution, so before moving to it, the best approach is to find the niche requirement and then find solution to the root problem. Proper synchronization between onshore and offshore team members, improved processes, and better time management and growth opportunities for both developed and developing countries are among few benefits that offshore software outsourcing companies are enjoying.
+
+Icreon, is an Offshore IT Outsourcing Company [http://www.icreon.com/], SAP business channel partner that savvy your business needs by providing elite Offshore Software Outsourcing solutions [http://www.icreon.com/outsourcing/offshore-software-development.shtml].
+
+Rakhi, is a experienced SEO content writer at Icreon Communications Pvt. Ltd.
+
+Article Source: http://EzineArticles.com/?expert=Rakhee_Chowdhary
+HERE
+
+article_3 = Hash.new
+article_3[:title] = "Web Based Accounting Software"
+article_3[:author_name] = "Jessica Shermin"
+article_3[:body] = <<HERE
+It is truly surprising what an accounting and bookkeeping software can do for a business. Many companies do not invest on servers and computer hardware/software since they think that making sense of their accounts is something that they can manage themselves. This is where they usually lose out on the profits that they could have been making if they had chosen to utilize web based accounting software to accommodate their financial records.
+
+Web based accounting application can greatly decrease the time it usually takes for business owners to evaluate their profit and loss systems. Budding entrepreneurs need cost effective bookkeeping solutions and such software is an easy means to acquire some. Since such programs are web hosted they operate with around the clock technical support to back them up. They also provide hands on training for personnel.
+
+An online invoice creator also saves business owners a lot of money. This is also one of the reasons why such web based software are becoming a fast growing trend for many small to medium size businesses. Keeping track of your and your customers' accounts can be a hassle if you don't know what you are doing. Web based accounting software can keep track of your bills. Creating a well thought out web based invoicing solution translates to instant results when it comes to your own profits.
+
+If you feel that your current account software will not suit your business' specific needs you will be happy to know that you also opt for customized accounting packages. These types of packages are usually utilized by large businesses and have served to save them a lot of money. Web based accounting software allows for a tangible accounting experience and also provides users with a secure accounting environment.
+
+Such software is also risk free since they learn and adapt from your needs and report on your status early. It can be made to work on any type of industry. This means that you can customize how you want it to report back to you. Web based accounting software are also helpful if you want to, say, shift from Windows to Linux.
+
+Perhaps what makes web based accounting application so integral to any business franchise is that it cannot be physically tainted. In other words, your financial records are safe as long as they remain on the World Wide Web. That way they will be safe from physical plunder.
+
+Our Company KashFlow provides easy to understand accounting software and bookkeeping software for small and medium size business, To find out more about our accounting software, please visit http://www.kashflow.co.uk
+
+Article Source: http://EzineArticles.com/?expert=Jessica_Shermin
+HERE
+
+Article.create([article_1, article_2, article_3])
