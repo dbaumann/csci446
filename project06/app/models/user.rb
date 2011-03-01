4b55455ee8caf8c0430e924a590cb671144dd626
Daniel Baumann <dbaumann@mines.edu>
2011-02-28 23:33:25 -0700

    Added User and UserSession models with AuthLogic.

---
+++ project06/app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  acts_as_authentic
+  
+  attr_accessible :username, :first_name, :last_name, :password,
+                  :password_confirmation, :email
+                  
+  validates_presence_of :username
+  validates_presence_of :email
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  
+end
